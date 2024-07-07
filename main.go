772ab2c2bbac755b655cb45a9ef30334df373574
sung <warrenchrisb@gmail.com>
2024-07-06 22:00:39 -0400

    weather struct

--- main.go
+++ main.go
@@ -1,13 +1,39 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
 )
 
+type Weather struct {
+	Location struct {
+		Name    string `json:"name"`
+		Country string `json:"country"`
+	} `json:"location"`
+	Current struct {
+		TempF     float64 `json:"temp_f"`
+		Condition struct {
+			Text string `json:"text"`
+		} `json:"condition"`
+	} `json:"current"`
+	Forecast struct {
+		Forecastday []struct {
+			Hour []struct {
+				TimeEpoch int64   `json"time_epoch"`
+				TempF     float64 `json"temp_f"`
+				Condition struct {
+					Text string `json:"text"`
+				} `json:"condition"`
+				ChanceOfRain float64 `json:"chance_of_rain"`
+			} `json:"hour"`
+		} `json:"forecastday'`
+	} `json:"forecast"`
+}
+
 func main() {
-	res, err := http.Get("http://api.weatherapi.com/v1/current.json?key=9020bf20706841f9abc05511240707&q=20001&aqi=no")
+	res, err := http.Get("http://api.weatherapi.com/v1/forecast.json?key=9020bf20706841f9abc05511240707&q=20001&days=1&aqi=no&alerts=no")
 	if err != nil {
 		panic(err)
 	}
@@ -22,5 +48,10 @@ func main() {
 		panic(err)
 	}
 
-	fmt.Println(string(body))
+	var weather Weather
+	err = json.Unmarshal(body, &weather)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(weather)
 }
