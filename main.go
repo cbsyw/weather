5f06985d53e5759e1ffdc8b315f68c271620c16e
sung <warrenchrisb@gmail.com>
2024-07-06 21:22:45 -0400

    add weather api/json

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
 
-func main(){
-	fmt.Println("ready to go")
+func main() {
+	res, err := http.Get("http://api.weatherapi.com/v1/current.json?key=9020bf20706841f9abc05511240707&q=20001&aqi=no")
+	if err != nil {
+		panic(err)
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != 200 {
+		panic("Weather API N/A")
+	}
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(string(body))
 }
